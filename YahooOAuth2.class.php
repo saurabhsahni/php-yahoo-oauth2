f44e960a5d225265c00b570368b40ccac76b2fa9
Bastien Ledon <bastien.ledon@yahoo.com>
2015-09-25 17:11:07 +0200

    fix Grant type cannot be null

--- YahooOAuth2.class.php
+++ YahooOAuth2.class.php
@@ -7,26 +7,26 @@ Find documentation and support on Yahoo Developer Network: https://developer.yah
 
 class YahooOAuth2 {
 
-  const AUTHORIZATION_ENDPOINT  = 'https://api.login.yahoo.com/oauth2/request_auth';
-  const TOKEN_ENDPOINT   = 'https://api.login.yahoo.com/oauth2/get_token';
+    const AUTHORIZATION_ENDPOINT  = 'https://api.login.yahoo.com/oauth2/request_auth';
+    const TOKEN_ENDPOINT   = 'https://api.login.yahoo.com/oauth2/get_token';
 
-  public function fetch($url,$postdata="",$auth="",$headers="") {
-        $curl = curl_init($url); 
-	if($postdata) {
-            curl_setopt($curl, CURLOPT_POST, true); 
-	    curl_setopt($curl, CURLOPT_POSTFIELDS, $postdata); 
-	} 
-	else {
-	    curl_setopt($curl, CURLOPT_POST, false);
-	}
-	if($auth){
-	    curl_setopt($curl, CURLOPT_USERPWD, $auth);
-	}
-	if($headers){
-	    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-	}
-	curl_setopt($curl, CURLOPT_HEADER, false);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true); 
+    public function fetch($url,$postdata="",$auth="",$headers="") {
+        $curl = curl_init($url);
+        if($postdata) {
+            curl_setopt($curl, CURLOPT_POST, true);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $postdata);
+        }
+        else {
+            curl_setopt($curl, CURLOPT_POST, false);
+        }
+        if($auth){
+            curl_setopt($curl, CURLOPT_USERPWD, $auth);
+        }
+        if($headers){
+            curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        }
+        curl_setopt($curl, CURLOPT_HEADER, false);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         $response = curl_exec( $curl );
         if (empty($response)) {
             // some kind of an error happened
@@ -42,24 +42,26 @@ class YahooOAuth2 {
             }
         }
         return $response;
-  }
-  public function getAuthorizationURL($client_id,$redirect_uri,$language="en-us") {
+    }
+    public function getAuthorizationURL($client_id,$redirect_uri,$language="en-us") {
         $url = self::AUTHORIZATION_ENDPOINT;
         $authorization_url=$url.'?'.'client_id='.$client_id.'&redirect_uri='.$redirect_uri.'&language='.$language.'&response_type=code';
         return $authorization_url;
-  }
+    }
 
 
-  public function get_access_token($clientId, $clientSecret,$redirect_uri,$code) {
+    public function get_access_token($clientId, $clientSecret,$redirect_uri,$code) {
         $url=self::TOKEN_ENDPOINT;
         $postdata=array("redirect_uri"=>$redirect_uri,"code"=>$code,"grant_type"=>"authorization_code");
-	$auth=$clientId . ":" . $clientSecret;
-        $response=self::fetch($url,$postdata,$auth); 
+        $fields_string = '';
+        foreach($postdata as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
+        $auth=$clientId . ":" . $clientSecret;
+        $response=self::fetch($url,$fields_string,$auth);
 
-	// Convert the result from JSON format to a PHP array 
-	$jsonResponse = json_decode( $response );
-	return $jsonResponse->access_token;
-  }
+        // Convert the result from JSON format to a PHP array
+        $jsonResponse = json_decode( $response );
+        return $jsonResponse->access_token;
+    }
 
 }
 
